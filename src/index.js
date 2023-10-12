63f3250ddcfc853c0c7ceb6ba991fdf9592286b3
Earl <earlmunyua96@gmail.com>
2023-10-12 08:44:06 +0300

    Tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const forms = document.querySelector("form")
+  forms.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    newTask(e.target["new-task-description"]["value"])
+    forms.reset()
+  })
 });
+function newTask(task) {
+  const p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", deleteTask)
+  btn.textContent = "x"
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
+
